8d4303f7e4e30ce533a22bd62ccb2be3c2bb5ff3
kamilsmr <kamils6035@gmail.com>
2021-12-13 01:01:56 +0300

    continue

--- tour_project/src/App.js
+++ tour_project/src/App.js
@@ -1,5 +1,12 @@
+import Navbar from "./components/navbar/Navbar";
+import "./App.css";
+
 function App() {
-  return <div className="App">Merhaba</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ tour_project/src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <a href="aboutus">ABOUT US</a>
+      <a href="foryou">FOR YOU</a>
+      <a href="services">SERVİCES</a>
+      <a href="blog">BLOG</a>
+      <a href="vlog">VLOG</a>
+      <a href="contact">CONTACT</a>
+    </div>
+  );
+};
+
+export default Navbar;
