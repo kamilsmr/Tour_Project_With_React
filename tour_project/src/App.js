0a076cea6e35d6a83e554b97413dbff1eaf2b9cd
kamilsmr <kamils6035@gmail.com>
2021-12-13 14:34:43 +0300

    header

--- tour_project/src/App.js
+++ tour_project/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/Navbar";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ tour_project/src/components/header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h3 className="big title">POPULAR TOUR PLACES</h3>
+    </div>
+  );
+};
+
+export default Header;
