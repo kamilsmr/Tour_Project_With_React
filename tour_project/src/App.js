bf3c709e1cfa64f60ad398cebf61abaae99e27de
kamilsmr <kamils6035@gmail.com>
2021-12-13 15:06:54 +0300

    data

--- tour_project/src/App.js
+++ tour_project/src/App.js
@@ -1,12 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import "./App.css";
 import Header from "./components/header/Header";
-
+import Cards from "./components/cards/Cards";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Cards />
     </div>
   );
 }
---
+++ tour_project/src/components/cards/Cards.js
@@ -0,0 +1,17 @@
+import "./Cards.css";
+import { data } from "../../helpers/data";
+
+console.log(data);
+
+const Cards = () => {
+  return (
+    <div>
+      <h2>Card Title</h2>
+    </div>
+    <img>
+    src =
+    </img>
+  );
+};
+
+export default Cards;
